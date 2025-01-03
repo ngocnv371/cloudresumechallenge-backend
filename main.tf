99a2fa7350380e582523c9556e18d2f9a80ccbbd
Ngoc Ngo <ngocnv371@gmail.com>
2025-01-03 00:54:02 -0800

    update terraform to set s3 website

--- main.tf
+++ main.tf
@@ -4,11 +4,43 @@ provider "aws" {
 
 resource "aws_s3_bucket" "frontend_bucket" {
   bucket = "ngocnv371-cloudresumechallenge-frontend"
-  acl    = "public-read"
+}
 
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
+resource "aws_s3_bucket_ownership_controls" "frontend_bucket" {
+  bucket = aws_s3_bucket.frontend_bucket.bucket
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "frontend_bucket" {
+  bucket = aws_s3_bucket.frontend_bucket.bucket
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "frontend_bucket" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.frontend_bucket,
+    aws_s3_bucket_public_access_block.frontend_bucket,
+  ]
+
+  bucket = aws_s3_bucket.frontend_bucket.bucket
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_website_configuration" "frontend_bucket" {
+  bucket = aws_s3_bucket.frontend_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
   }
 }
 
